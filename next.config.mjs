53ccccda2c3f8510da83ab62b7c6834cc8b04f14
Larissa Barcellos Ferreira <132204048+larissadcew@users.noreply.github.com>
2024-10-26 09:01:16 -0300

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['public.blob.vercel-storage.com'],
+    },
+  }
+  
+  export default nextConfig;
+  
